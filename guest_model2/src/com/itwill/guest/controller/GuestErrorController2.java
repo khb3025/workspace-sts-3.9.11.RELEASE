7564453ff5ca6dd8e4a878a06849f25da0e2c29d
itwillclass5 <59531538+itwillclass5@users.noreply.github.com>
2020-02-12 20:49:01 +0900

    .

---
+++ guest_model2/src/com/itwill/guest/controller/GuestErrorController2.java
@@ -0,0 +1,18 @@
+package com.itwill.guest.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.itwill.summer.Controller;
+
+public class GuestErrorController2 implements Controller {
+
+	@Override
+	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
